2863b967e4f09d6cd8be65cb4ad9892ab61ca10a
suu1006 <1006pp@naver.com>
2025-12-01 16:28:14 +0900

    [Fix] 기본 다크모드로 변경

--- app/store/use-theme.ts
+++ app/store/use-theme.ts
@@ -12,7 +12,7 @@ interface ThemeState {
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
-      theme: "light" as Theme,
+      theme: "dark" as Theme, // 기본 dark 테마
       hasHydrated: false,
       toggle: () =>
         set((state) => ({
